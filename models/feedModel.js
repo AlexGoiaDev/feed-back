fd257d7b62aeb932e730865b4a808a57c1dca0e8
System Administrator <alexgoiadev@gmail.com>
2019-12-22 10:36:53 +0100

    Añadido el modelado

---
+++ models/feedModel.js
@@ -0,0 +1,28 @@
+let mongoose = require('mongoose');
+let mongoosePaginate = require('mongoose-paginate-v2');
+
+const Feed = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    source: {
+        type: String,
+        required: true
+    },
+    publisher: {
+        type: String,
+        required: true
+    }
+});
+
+Feed.plugin(mongoosePaginate);
+module.exports = mongoose.model('Feed', Feed);
